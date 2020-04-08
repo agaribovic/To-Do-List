b24043e98b1a68810d0a100223eddd0d7f4a1659
agaribovic <a.garibovic95@gmail.com>
2020-04-08 15:03:29 +0200

    added new date format

--- javascript.js
+++ javascript.js
@@ -1,7 +1,78 @@
 document.getElementById("clear").style.display = "none";
 document.getElementById("checkBox").style.display = "none";
 document.getElementById("selectAll").style.display = "none";
-document.getElementById("date").innerHTML = new Date();
+
+let date = new Date();
+let weekDay = date.getDay();
+let monthDay = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+switch (weekDay) {
+  case 0:
+    weekDay = "Sunday";
+    break;
+  case 1:
+    weekDay = "Monday";
+    break;
+  case 2:
+    weekDay = "Tuesday";
+    break;
+  case 3:
+    weekDay = "Wednesday";
+    break;
+  case 4:
+    weekDay = "Thursday";
+    break;
+  case 5:
+    weekday = "Friday";
+    break;
+  case 6:
+    weekday = "Saturday";
+    break;
+}
+
+switch (month) {
+  case 0:
+    month = "January";
+    break;
+  case 1:
+    month = "February";
+    break;
+  case 2:
+    month = "March";
+    break;
+  case 3:
+    month = "April";
+    break;
+  case 4:
+    month = "May";
+    break;
+  case 5:
+    month = "June";
+    break;
+  case 6:
+    month = "July";
+    break;
+  case 7:
+    month = "August";
+    break;
+  case 8:
+    month = "September";
+    break;
+  case 9:
+    month = "October";
+    break;
+  case 10:
+    month = "November";
+    break;
+  case 11:
+    month = "December";
+    break;
+}
+
+let dateToday = weekDay + ", " + monthDay + " " + month + " " + year;
+document.getElementById("date").innerHTML = dateToday;
 
 document.getElementById("input").addEventListener("keyup", function (e) {
   let input = document.getElementById("input").value;
@@ -33,7 +104,9 @@ document.getElementById("input").addEventListener("keyup", function (e) {
       function deleteTask(event) {
         this.parentElement.style.display = "none";
 
-        if (document.getElementById("lista").lastChild.style.display == "none") {
+        if (
+          document.getElementById("lista").lastChild.style.display == "none"
+        ) {
           button.style.display = "none";
           selectAll.style.display = "none";
           checkBox.style.display = "none";
